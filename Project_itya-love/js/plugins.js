a2c1b5d2afc9aff45a57d5a89111ba61f65b9cb0
harulila <91677500+harulila@users.noreply.github.com>
2021-11-12 13:01:33 +0900

    plugin
    基本的なプラグイン入れた

--- Project_itya-love/js/plugins.js
+++ Project_itya-love/js/plugins.js
@@ -3,5 +3,11 @@
 var $plugins =
 [
 {"name":"Community_Basic","status":true,"description":"基本的なパラメーターを設定するプラグインです。","parameters":{"cacheLimit":"20","screenWidth":"816","screenHeight":"624","changeWindowWidthTo":"","changeWindowHeightTo":"","renderingMode":"auto","alwaysDash":"off"}},
-{"name":"MadeWithMv","status":true,"description":"メイン画面へ進む前に、\"Made with MV\"のスプラッシュ画面もしくはカスタマイズされたスプラッシュ画面を表示します。","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}}
+{"name":"MadeWithMv","status":true,"description":"メイン画面へ進む前に、\"Made with MV\"のスプラッシュ画面もしくはカスタマイズされたスプラッシュ画面を表示します。","parameters":{"Show Made With MV":"true","Made with MV Image":"MadeWithMv","Show Custom Splash":"false","Custom Image":"","Fade Out Time":"120","Fade In Time":"120","Wait Time":"160"}},
+{"name":"Yami_SkipTitle","status":true,"description":"テストプレイ用にタイトルシーンを飛ばすことができます。","parameters":{}},
+{"name":"UTA_MessageSkip","status":true,"description":"特定キーを押す事でメッセージをスキップできるようにします。","parameters":{"Skip Key":"control","Show Trace":"false"}},
+{"name":"AudioStreaming","status":true,"description":"音声読み込みを高速化し、oggファイルのみを使用します。","parameters":{"mode":"10","deleteM4a":"false"}},
+{"name":"MPP_MapLight","status":true,"description":"マップの明るさを設定できるようにします。","parameters":{"Light Colors":"[\"255,255,255\",\"192,128,64\",\"32,32,32\"]","Front Light":"{\"Radius\":\"6\",\"Angle\":\"90\",\"Oy\":\"4\",\"Turn Duration\":\"24\"}","Darkness Size":"3"}},
+{"name":"MessageWindowHidden","status":true,"description":"メッセージウィンドウ一時消去プラグイン","parameters":{"triggerButton":"[\"右クリック\"]","triggerSwitch":"0","linkPictureNumbers":"[]","linkShowPictureNumbers":"[]","disableLinkSwitchId":"0","disableSwitchId":"0","disableInBattle":"false","disableInChoice":"true"}},
+{"name":"SAN_AnalogMove","status":true,"description":"アナログムーブ 3.1.5\r\nパーティキャラクターの移動をドット移動に変更します。","parameters":{}}
 ];
